1d2e09a0ac36dd0006f8595fde78e3af44407d4b
bebop-cow <tyrinfern@yahoo.com>
2023-05-17 22:14:22 -0400

    updated html, added css and js

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var toggleContainer = document.getElementById("toggle-container");
+
+  for (var i = 0; i < 1000; i++) {
+    var toggleBox = createToggleBox(i + 1);
+    toggleContainer.appendChild(toggleBox);
+  }
+
+  function createToggleBox(index) {
+    var toggleBox = document.createElement("div");
+    toggleBox.classList.add("toggle-box");
+
+    var label = document.createElement("label");
+    label.classList.add("toggle-label");
+    label.innerHTML = "Toggle " + index;
+    label.addEventListener("click", function() {
+      toggleContent.style.display = (toggleContent.style.display === "none") ? "block" : "none";
+    });
+
+    var toggleContent = document.createElement("div");
+    toggleContent.classList.add("toggle-content");
+    toggleContent.innerHTML = "Content " + index;
+
+    toggleBox.appendChild(label);
+    toggleBox.appendChild(toggleContent);
+
+    return toggleBox;
+  }
+});
