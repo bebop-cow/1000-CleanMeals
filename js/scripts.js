e08adc31afb845f2f49d987d40747936097f9f3d
bebop-cow <tyrinfern@yahoo.com>
2023-06-05 16:04:15 -0400

    toggle correction

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     label.innerHTML = "Meal " + index;
     label.addEventListener("click", function() {
       toggleContent.style.display = (toggleContent.style.display === "none") ? "block" : "none";
-      label.classList.add("blue");
+      toggle-box.classList.add("blue");
     });
 
     var toggleContent = document.createElement("div");
